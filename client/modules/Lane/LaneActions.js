82adc66ca943fc8fe680a12833690d8021fcbfb6
Damian Juzyk <damian.juzyk@gmail.com>
2018-03-10 12:52:27 +0400

    add CRUD for Notes

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,3 +1,36 @@
+import uuid from 'uuid'
+
 // Export Constants
+export const CREATE_LANE = 'CREATE_LANE';
+export const UPDATE_LANE = 'UPDATE_LANE';
+export const DELETE_LANE = 'DELETE_LANE';
 
 // Export Actions
+
+// add a Lane
+export function createLane(lane) {
+    return {
+      type: CREATE_LANE,
+      lane: {
+        id: uuid(),
+        notes: [],
+        ...lane,
+      }
+    };
+  }
+
+  //update a Lane
+  export function updateLane(lane) {
+    return {
+      type: UPDATE_LANE,
+      lane,
+    };
+  }
+
+  //delete a Lane
+  export function deleteLane(laneId) {
+    return {
+      type: DELETE_LANE,
+      laneId
+    };
+  }
