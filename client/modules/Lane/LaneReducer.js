606b8118e45a2d46d10c0a45256bb07ac689d54b
Damian Juzyk <damian.juzyk@gmail.com>
2018-03-10 13:17:02 +0400

    add LaneReducers

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,14 +1,30 @@
 // Import Actions
-import {  } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
 
 // Initial State
 const initialState = {};
 
-const LaneReducer = (state = initialState, action) => {
+export default function lanes(state = initialState, action) {
   switch (action.type) {
+    case CREATE_LANE:
+      return [...state, action.name];
+      case UPDATE_LANE:
+      return state.map(lane => {
+        return lane.id === action.id ? { ...lane, ...action.lane } : lane;
+      });
+    case DELETE_LANE:
+      return state.filter(lane => lane.id !== action.laneId);
+    
     default:
       return state;
-  }
-};
+    }
+}
+
+// const LaneReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     default:
+//       return state;
+//   }
+// };
 
 export default LaneReducer;
--- client/modules/Note/NoteActions.js
+++ client/modules/Note/NoteActions.js
@@ -1,3 +1,37 @@
+import uuid from 'uuid'
+
 // Export Constants
+export const CREATE_NOTE = 'CREATE_NOTE';
+export const UPDATE_NOTE = 'UPDATE_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
 
 // Export Actions
+// add a Note
+export function createNote(note, laneId) {
+    return {
+      type: CREATE_NOTE,
+      laneId,
+      note: {
+        id: uuid(),
+        ...note,
+      },
+    };
+  }
+
+  // update a Note
+  export function updateNote(note) {
+    return {
+      type: UPDATE_NOTE,
+      note,
+    };
+  }
+  
+
+  // delete a Note
+  export function deleteNote(noteId, laneId) {
+    return {
+      type: DELETE_NOTE,
+      noteId,
+      laneId,
+    };
+  }
