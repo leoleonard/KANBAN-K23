8d0c82e4727db6c73aa372d2717e1f0aa0318442
Damian Juzyk <damian.juzyk@gmail.com>
2018-03-10 13:37:54 +0400

    add Component P Lane

---
+++ client/modules/Lane/Lanes.js
@@ -0,0 +1,18 @@
+// wyświetla pojedynczą linię
+
+import React, { PropTypes } from 'react';
+import Lane from './Lane.js';
+
+const Lanes = ({ lanes }) => {
+  return (
+    <div className="lanes">{lanes.map(lane =>
+      <Lane className="lane" key={lane.id} lane={lane} />
+    )}</div>
+  );
+};
+
+Lanes.propTypes = {
+  lanes: PropTypes.array,
+};
+
+export default Lanes;
