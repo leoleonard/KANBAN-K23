4c378b78c21d81c406f74e6b04ad8e9e82f42b74
Damian Juzyk <damian.juzyk@gmail.com>
2018-03-10 12:12:23 +0400

    add post a new Note

--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import * as NoteController from '../controllers/note.controller';
 
-import uuid from 'uuid';
-
 const router = new Router();
 
+// add a Note
+router.route('/notes').post(NoteController.addNote);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ import routes from '../client/routes';
 import { fetchComponentData } from './util/fetchData';
 // import posts from './routes/post.routes';
 import lanes from './routes/lane.routes';
+import notes from './routes/note.routes';
 import dummyData from './dummyData';
 import serverConfig from './config';
 
@@ -62,6 +63,7 @@ app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(Express.static(path.resolve(__dirname, '../dist/client')));
 // app.use('/api', posts);
 app.use('/api', lanes);
+app.use('/api', notes);
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
