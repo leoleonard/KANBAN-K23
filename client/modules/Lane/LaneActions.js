27edb580343321599a982b2dc256052a1c03e281
Damian Juzyk <damian.juzyk@gmail.com>
2018-03-10 13:23:43 +0400

    add NoteReducers

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -34,3 +34,5 @@ export function createLane(lane) {
       laneId
     };
   }
+
+  
--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,5 +1,6 @@
 // Import Actions
 import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
 
 // Initial State
 const initialState = {};
@@ -8,13 +9,34 @@ export default function lanes(state = initialState, action) {
   switch (action.type) {
     case CREATE_LANE:
       return [...state, action.name];
-      case UPDATE_LANE:
+
+    case UPDATE_LANE:
       return state.map(lane => {
         return lane.id === action.id ? { ...lane, ...action.lane } : lane;
       });
+      
     case DELETE_LANE:
       return state.filter(lane => lane.id !== action.laneId);
     
+    case CREATE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return { ...lane, notes };
+        }
+        return lane;
+      });
+
+      case DELETE_NOTE:
+        return state.map(lane => {
+          if (lane.id === action.laneId) {
+    
+            const notes = lanes.notes ? lane.notes.filter(note => note.id !== action.noteId) : [];
+            return { ...lane, notes };
+          }
+        return lane;  
+        });
+
     default:
       return state;
     }
@@ -27,4 +49,4 @@ export default function lanes(state = initialState, action) {
 //   }
 // };
 
-export default LaneReducer;
+// export default LaneReducer;
